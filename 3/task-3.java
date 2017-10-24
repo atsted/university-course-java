2caf256564f4eb65009621792fd69c51b6a0878b
Acamaz Tedeti <atsatedeev@gmail.com>
2017-10-25 00:36:46 +0300

    Add Task 3/3

--- 3/task-3.java
+++ 3/task-3.java
@@ -7,12 +7,21 @@ class Program {
   }
   public static void main(String[] args) {
     int[] arr = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-    int index;
-    double value;
-    System.out.print("Введите индекс элемента:\t");
-    index = Integer.parseInt(System.console().readLine());
-    System.out.print("Введите значение элемента:\t");
-    value = Double.parseInt(System.console().readLine());
-    Program.printArray(arr);
+    int index, value, len = arr.length;
+    for (;;) {
+      System.out.print("Введите индекс элемента:\t");
+      index = Integer.parseInt(System.console().readLine());
+      System.out.print("Введите значение элемента:\t");
+      value = Integer.parseInt(System.console().readLine());
+      if (index < 0 || index >= len) {
+        System.out.printf("Значение индекса должно лежать в интревале [0;%d]\n", len - 1);
+        continue;
+      } else {
+        arr[index] = value;
+        System.out.print("Массив:\t"); Program.printArray(arr);
+      }
+      System.out.print("Вы хотите продолжить? [y/n] ");
+      if (System.console().readLine().equals("n")) break;
+    }
   }
 }
